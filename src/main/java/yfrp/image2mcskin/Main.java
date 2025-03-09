96c1ec19f35bd90df7cf7f54908230a80ae74d71
stu <stupickaxe@outlook.com>
2025-03-09 13:51:45 +0800

    update

---
+++ src/main/java/yfrp/image2mcskin/Main.java
@@ -0,0 +1,14 @@
+package yfrp.image2mcskin;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        -i <导入的文件位置> -o <导出的文件位置>
+         */
+        for (int i = 0; i < args.length; i++) {
+            if (i < args.length - 1 && args[i].equals("-i")) {
+
+            }
+        }
+    }
+}
