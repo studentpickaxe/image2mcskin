73c1e5ec059724393cb00698e96f585deb82bd56
stu <stupickaxe@outlook.com>
2025-04-19 23:14:42 +0800

    Update 删除 Main 类中多余的 IOException

--- src/main/java/yfrp/image2mcskin/Main.java
+++ src/main/java/yfrp/image2mcskin/Main.java
@@ -1,10 +1,9 @@
 package yfrp.image2mcskin;
 
 import java.io.File;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try {
 
             Arguments arguments = Arguments.fromStringArray(args);
