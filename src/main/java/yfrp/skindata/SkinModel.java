12b2bbc9989c0007a2f43f7b77bb940f1c39d666
stu <stupickaxe@outlook.com>
2025-04-22 23:35:22 +0800

    Update add getCubes() method

--- src/main/java/yfrp/skindata/SkinModel.java
+++ src/main/java/yfrp/skindata/SkinModel.java
@@ -1,9 +1,16 @@
 package yfrp.skindata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record SkinModel(ModelCube cubeHead,
                         ModelCube cubeBody,
                         ModelCube cubeArmR,
                         ModelCube cubeArmL,
                         ModelCube cubeLegR,
                         ModelCube cubeLegL) {
+
+    public ModelCube[] getCubes() {
+        return new ModelCube[]{cubeHead, cubeBody, cubeArmR, cubeArmL, cubeLegR, cubeLegL};
+    }
 }
